c1f648e0bc5c1c4fe220c3ff9c0917a8adaf87d2
Marco Pastorello <marco.pastorello@capgemini.com>
2026-09-01 16:59:27 +0200

    Updated

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,4 +3,4 @@ export const greetGuest = (name: string) => `Che bello averti con noi, ${name}!`
 
 export const WEDDING_DATE = '10 settembre 2027'
 export const WEDDING_LOCATION = 'Costa Ponente'
-export const WEDDING_MAPS_URL = 'https://share.google/5fPPFXrFDt4aqlwJv'
+export const WEDDING_MAPS_URL = 'https://www.agoracatering.it/costa-ponente'
